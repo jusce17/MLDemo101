d5e971e1aed78b6baef012f74c8294a5b5706801
Eden Juscelino <eden.juscelino@gmail.com>
2019-06-16 14:10:54 +0100

    Initial Commit

---
+++ Core ML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core ML
+//
+//  Created by Eden Juscelino on 16/06/2019.
+//  Copyright © 2019 Eden Juscelino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
